84cd4f5baa0de58756d5b962892766987e295533
Warley Kennedy <warley-ft@hotmail.com>
2022-01-28 20:33:47 -0300

    Primeiro Commit

---
+++ src/main/java/br/com/revistainfoco/revista/RevistaApplication.java
@@ -0,0 +1,13 @@
+package br.com.revistainfoco.revista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevistaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevistaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/revistainfoco/revista/RevistaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.revistainfoco.revista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevistaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
